d4a7f316f68a06fefc0191f153e392bcc6fcb02e
Koprowski <koprowski@gmail.com>
2024-11-03 10:07:21 -0500

    Updated auth routes for production

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,15 +8,17 @@ router.get('/google',
 );
 
 router.get('/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  (req, res) => {
-    res.redirect('/dashboard');
-  }
+  passport.authenticate('google', { 
+    failureRedirect: '/login',
+    successRedirect: '/dashboard'
+  })
 );
 
 router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
